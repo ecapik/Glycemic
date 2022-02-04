5fed86c6a3e61fed671e5a767fecd841123f91c0
Erencan Capik <erencancapik@icloud.com>
2022-02-04 19:07:13 +0300

    Initial commit

---
+++ src/main/java/com/work/glycemic/GlycemicApplication.java
@@ -0,0 +1,13 @@
+package com.work.glycemic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlycemicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlycemicApplication.class, args);
+    }
+
+}
